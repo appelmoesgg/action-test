0cd6bd315c6986e01503df63694292812ad1bd50
appelmoesGG <101522959+appelmoesgg@users.noreply.github.com>
2025-02-05 14:39:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ real_key = sys.argv[2]
 
 print("Hi everyone super secret key program here")
 print(f"The env key variable: {key}")
-print(f"The real key: {real_key}")
-print("If I didn't print the real key, no one would have known what it was...")
+if real_key = "THIS_IS_THE_REAL_KEY_OFC":
+    print(f"The real secrets keys match!")
