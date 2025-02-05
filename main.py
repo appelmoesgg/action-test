ddbb38b53127e3f32210c339f3d3c52c72bc6f08
appelmoesGG <101522959+appelmoesgg@users.noreply.github.com>
2025-02-05 14:14:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World") #Fire coding
+import sys
+
+key = sys.argv[1]
+real_key = sys.argv[2]
+
+print("Hi everyone super secret key program here")
+print(f"The env key variable: {key}")
+print(f"The real key: {real_key}")
+print("If I didn't print the real key, no one would have known what it was...")
